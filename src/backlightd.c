1129da437f5083dd4200407af42f51389395da10
leventerevesz <levete.revesz@gmail.com>
2020-05-16 22:54:02 +0200

    Add openlog

--- src/backlightd.c
+++ src/backlightd.c
@@ -4,7 +4,18 @@
 #include "backlightd.h"
 #include "timecalc.h"
 
+#include <stdio.h>
+
 int main()
 {
+    openlog("backlightd", 0, LOG_DAEMON);
+
+    config_t __config;
+    config_handle_t config = &__config;
+    read_config(CONFIG_PATH, config);
+
+    syslog(LOG_INFO, "Backlightd starting up...\n");
+
     return EXIT_SUCCESS;
+    closelog();
 }
